5977992c5b258c9b55b448d0dfce3810dde8f0ab
l__o_ <genius8111@naver.com>
2024-11-01 15:03:26 +0900

    Feat: first commit

---
+++ src/test/java/exam/parkReview/ParkReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.parkReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
