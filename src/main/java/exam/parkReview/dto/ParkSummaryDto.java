eb88a1646156a5117d139b7f5c9f4f397f263f72
l__o_ <genius8111@naver.com>
2024-11-04 22:33:09 +0900

    Feat: 전체 공원 조회 dto

---
+++ src/main/java/exam/parkReview/dto/ParkSummaryDto.java
@@ -0,0 +1,24 @@
+package exam.parkReview.dto;
+
+import exam.parkReview.domain.entity.Park;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ParkSummaryDto {
+
+    private long parkNum;
+    private String parkName;
+    private String parkSummary;
+    private String address;
+
+    public ParkSummaryDto(Park park) {
+        this.parkNum = park.getParkNum();
+        this.parkName = park.getParkName();
+        this.parkSummary = park.getParkSummary();
+        this.address = park.getAddress();
+    }
+}
