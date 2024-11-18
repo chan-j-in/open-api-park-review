cf22a583b618cd052aa2ce428c5fa3682f7ccdc9
l__o_ <genius8111@naver.com>
2024-11-18 13:33:29 +0900

    Feat: 회원 도메인 개발

---
+++ src/main/java/exam/parkReview/domain/entity/Member.java
@@ -0,0 +1,29 @@
+package exam.parkReview.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @Builder
+    public Member(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
