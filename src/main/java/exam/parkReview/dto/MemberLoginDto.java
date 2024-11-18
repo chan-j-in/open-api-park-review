cf400c924764b288e9e337edd840d560e42dbdfd
l__o_ <genius8111@naver.com>
2024-11-18 13:34:31 +0900

    Feat: login dto

---
+++ src/main/java/exam/parkReview/dto/MemberLoginDto.java
@@ -0,0 +1,14 @@
+package exam.parkReview.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberLoginDto {
+
+    private String username;
+    private String password;
+}
