b01c8b32db2b5fa5aeeca2fe61030dea865f7ca7
l__o_ <genius8111@naver.com>
2024-11-18 13:34:13 +0900

    Feat: 로그인/로그아웃 구현(jwt사용X)

---
+++ src/main/java/exam/parkReview/controller/MemberController.java
@@ -0,0 +1,34 @@
+package exam.parkReview.controller;
+
+
+import exam.parkReview.dto.MemberLoginDto;
+import exam.parkReview.dto.MemberSignupDto;
+import exam.parkReview.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUp(@RequestBody MemberSignupDto memberSignupDto) {
+
+        memberService.signUp(memberSignupDto);
+        return ResponseEntity.ok().body("회원가입 성공");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody MemberLoginDto memberLoginDto) {
+
+        String token = memberService.login(memberLoginDto);
+        return ResponseEntity.ok().body(token);
+    }
+}
---
+++ src/main/java/exam/parkReview/service/MemberService.java
@@ -0,0 +1,47 @@
+package exam.parkReview.service;
+
+import exam.parkReview.domain.entity.Member;
+import exam.parkReview.dto.MemberLoginDto;
+import exam.parkReview.dto.MemberSignupDto;
+import exam.parkReview.exception.AppException;
+import exam.parkReview.exception.ErrorCode;
+import exam.parkReview.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void signUp(MemberSignupDto memberSignupDto) {
+
+        if (memberRepository.existsByUsername(memberSignupDto.getUsername())) {
+            throw new AppException(ErrorCode.DUPLICATE_MEMBER, "중복된 ID 입니다.");
+        }
+
+        if(memberSignupDto.getPassword().equals(memberSignupDto.getPasswordCheck())) {
+            throw new AppException(ErrorCode.PASSWORD_DO_NOT_MATCH, "두 비밀번호가 일치하지 않습니다.");
+        }
+
+        Member member = Member.builder()
+                .username(memberSignupDto.getUsername())
+                .password(memberSignupDto.getPassword())
+                .build();
+        memberRepository.save(member);
+    }
+
+    public String login(MemberLoginDto memberLoginDto) {
+
+        Member member = memberRepository.findByUsername(memberLoginDto.getUsername())
+                .orElseThrow(() -> new AppException(ErrorCode.INVALID_CREDENTIALS, "아이디 또는 비밀번호를 확인해주세요."));
+
+        if (member.getPassword().equals(memberLoginDto.getPassword())) {
+            throw new AppException(ErrorCode.INVALID_CREDENTIALS, "아이디 또는 비밀번호를 확인해주세요.");
+        }
+
+        return "token";
+    }
+
+}
