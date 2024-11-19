b5e006ba95d168f51388ee8df385d5bc258089b5
l__o_ <genius8111@naver.com>
2024-11-20 02:11:46 +0900

    Feat: member 조회 구현

--- src/main/java/exam/parkReview/controller/MemberController.java
+++ src/main/java/exam/parkReview/controller/MemberController.java
@@ -1,9 +1,16 @@
 package exam.parkReview.controller;
 
+import exam.parkReview.dto.member.MemberDetailDto;
+import exam.parkReview.dto.member.MemberDto;
 import exam.parkReview.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,4 +19,17 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @GetMapping
+    public ResponseEntity<List<MemberDto>> getMembers() {
+
+        List<MemberDto> members = memberService.getMembers();
+        return ResponseEntity.ok(members);
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberDetailDto> getMemberDetail(@PathVariable("memberId") Long memberId) {
+
+        MemberDetailDto memberDetail = memberService.getMemberDetail(memberId);
+        return ResponseEntity.ok(memberDetail);
+    }
 }
--- src/main/java/exam/parkReview/service/MemberService.java
+++ src/main/java/exam/parkReview/service/MemberService.java
@@ -1,14 +1,17 @@
 package exam.parkReview.service;
 
 import exam.parkReview.domain.entity.Member;
-import exam.parkReview.dto.MemberLoginDto;
-import exam.parkReview.dto.MemberSignupDto;
+import exam.parkReview.dto.member.MemberDetailDto;
+import exam.parkReview.dto.member.MemberDto;
 import exam.parkReview.exception.AppException;
 import exam.parkReview.exception.ErrorCode;
 import exam.parkReview.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,4 +20,28 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public List<MemberDto> getMembers() {
+
+        List<Member> members = memberRepository.findAll();
+
+        return members.stream()
+                .map(member -> new MemberDto(
+                        member.getUsername(),
+                        member.getReviewCount(),
+                        member.getRatingAvg()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    public MemberDetailDto getMemberDetail(Long memberId) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new AppException(ErrorCode.MEMBER_NOT_FOUND, "회원을 찾을 수 없습니다."));
+
+        return new MemberDetailDto(
+                member.getUsername(),
+                member.getReviewCount(),
+                member.getRatingAvg(),
+                member.getReviews());
+    }
 }
