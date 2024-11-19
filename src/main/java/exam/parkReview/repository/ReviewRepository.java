e3961b9ac2bb7034c527e0fd6ce5e645dc01812b
l__o_ <genius8111@naver.com>
2024-11-19 20:19:07 +0900

    Feat: park와 member로 조회 추가

--- src/main/java/exam/parkReview/repository/ReviewRepository.java
+++ src/main/java/exam/parkReview/repository/ReviewRepository.java
@@ -6,9 +6,12 @@ import exam.parkReview.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    Optional<Review> findByParkAndMember(Park park, Member member);
     List<Review> findByPark(Park park);
     List<Review> findByMember(Member member);
+    boolean existsByParkAndMember(Park park, Member member);
 }
