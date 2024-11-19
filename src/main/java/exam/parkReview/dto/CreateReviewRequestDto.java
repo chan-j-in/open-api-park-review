48b364a72c18d1294e36f9ae9e83c7c32bd7f739
l__o_ <genius8111@naver.com>
2024-11-19 20:12:26 +0900

    Feat: 리뷰작성 request dto

---
+++ src/main/java/exam/parkReview/dto/CreateReviewRequestDto.java
@@ -0,0 +1,21 @@
+package exam.parkReview.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateReviewRequestDto {
+
+    @NotBlank(message = "내용을 입력해주세요.")
+    @Size(max = 100, message = "리뷰 내용은 최대 100자까지 입력 가능합니다.")
+    private String content;
+
+    @NotNull(message = "평점을 입력해주세요.")
+    @Min(value = 1, message = "평점은 최소 1점이어야 합니다.")
+    @Max(value = 5, message = "평점은 최대 5점이어야 합니다.")
+    private int rating;
+}
