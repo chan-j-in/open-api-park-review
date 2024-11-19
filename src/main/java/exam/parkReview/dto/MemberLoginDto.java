990e61278d62d9f4348280527029ffc58e265c84
l__o_ <genius8111@naver.com>
2024-11-19 20:13:22 +0900

    Fix: validation 추가

--- src/main/java/exam/parkReview/dto/MemberLoginDto.java
+++ src/main/java/exam/parkReview/dto/MemberLoginDto.java
@@ -1,5 +1,6 @@
 package exam.parkReview.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberLoginDto {
 
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String username;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
--- src/main/java/exam/parkReview/dto/MemberSignupDto.java
+++ src/main/java/exam/parkReview/dto/MemberSignupDto.java
@@ -1,5 +1,7 @@
 package exam.parkReview.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberSignupDto {
 
+    @NotBlank(message = "이름은 필수 입력값입니다.")
     private String username;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Size(min = 4, message = "비밀번호는 최소 4자리 이상이어야 합니다.")
     private String password;
+
+    @NotBlank(message = "비밀번호 확인은 필수 입력값입니다.")
     private String passwordCheck;
 }
