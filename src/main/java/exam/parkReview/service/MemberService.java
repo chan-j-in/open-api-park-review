46c339efa5f0d4e0cdded455e6788da86e1ef63e
l__o_ <genius8111@naver.com>
2024-11-21 02:07:24 +0900

    Fix: 순환참조 오류 수정(dto)

--- src/main/java/exam/parkReview/service/MemberService.java
+++ src/main/java/exam/parkReview/service/MemberService.java
@@ -1,11 +1,14 @@
 package exam.parkReview.service;
 
 import exam.parkReview.domain.entity.Member;
+import exam.parkReview.domain.entity.Review;
 import exam.parkReview.dto.member.MemberDetailDto;
 import exam.parkReview.dto.member.MemberDto;
+import exam.parkReview.dto.review.ReviewResponseDto;
 import exam.parkReview.exception.AppException;
 import exam.parkReview.exception.ErrorCode;
 import exam.parkReview.repository.MemberRepository;
+import exam.parkReview.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +22,7 @@ import java.util.stream.Collectors;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final ReviewRepository reviewRepository;
 
     public List<MemberDto> getMembers() {
 
@@ -38,11 +42,21 @@ public class MemberService {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new AppException(ErrorCode.MEMBER_NOT_FOUND, "회원을 찾을 수 없습니다."));
 
+        List<Review> reviews = reviewRepository.findByMember(member);
+        List<ReviewResponseDto> reviewResponseDtos = reviews.stream()
+                .map(review -> new ReviewResponseDto(
+                        review.getPark().getParkNum(),
+                        review.getContent(),
+                        review.getRating(),
+                        review.getMember().getUsername()
+                ))
+                .collect(Collectors.toList());
+
         return new MemberDetailDto(
                 member.getUsername(),
                 member.getReviewCount(),
                 member.getRatingAvg(),
-                member.getReviews());
+                reviewResponseDtos);
     }
 
     public void deleteMember(Long memberId) {
